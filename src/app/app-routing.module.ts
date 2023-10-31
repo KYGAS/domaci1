35eb8e4edca5b990d25a00b527a29e942ce989e3
KYGAS <acazvezdas@gmail.com>
2023-10-31 22:21:05 +0100

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { EntityExtractionComponent } from './entity-extraction/entity-extraction.component';
+import { LanguageDetectionComponent } from './language-detection/language-detection.component';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
+import { TextSimilarityComponent } from './text-similarity/text-similarity.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'entity-extraction', component: EntityExtractionComponent },
+  { path: 'language-detection', component: LanguageDetectionComponent },
+  { path: 'sentiment-analysis', component: SentimentAnalysisComponent },
+  { path: 'text-similarity', component: TextSimilarityComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { EntityExtractionComponent } from './entity-extraction/entity-extraction.component';
+import { TextSimilarityComponent } from './text-similarity/text-similarity.component';
+import { LanguageDetectionComponent } from './language-detection/language-detection.component';
+import { SentimentAnalysisComponent } from './sentiment-analysis/sentiment-analysis.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    EntityExtractionComponent,
+    TextSimilarityComponent,
+    LanguageDetectionComponent,
+    SentimentAnalysisComponent
   ],
   imports: [
     BrowserModule,
